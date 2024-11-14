259a7c5cf90ade958123e3ee695d642cc05aa171
darius1751 <lupequi12@gmail.com>
2024-11-14 11:56:18 -0500

    Change cookies by localstorage

--- src/services/login.ts
+++ src/services/login.ts
@@ -3,6 +3,6 @@ import { api } from "../config/api";
 import { User } from "../interfaces/user";
 
 export const login = async (credential: Credential) => {
-    const { data, status } = await api.post<User>('/auth/login', credential);
+    const { data, status } = await api.post<{ user: User, token: string }>('/auth/login', credential);
     return { data, status }
 }
