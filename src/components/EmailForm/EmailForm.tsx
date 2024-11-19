fbd3004b93cd074ad7e7e7e041764d6ab4730d7b
Dani <dustyglass@gmail.com>
2024-11-19 13:42:52 -0300

    add friend name

--- src/components/EmailForm/EmailForm.tsx
+++ src/components/EmailForm/EmailForm.tsx
@@ -11,12 +11,13 @@ interface EmailFormProps {
     const [sender, setSender] = useState('');
     const [receiver, setReceiver] = useState('');
     const [name, setName] = useState('');
+    const [friend, setFriend] = useState('');
   
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const handleSubmit = (e: any) => {
       e.preventDefault();
       try {
-        sendBookEmail(sender, receiver, name, id)
+        sendBookEmail(sender, receiver, name, friend, id)
         toggle()
       } catch (error) {
         console.log(error)
@@ -42,6 +43,12 @@ interface EmailFormProps {
           placeholder="Your Name"
           value={name}
           onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Friend's Name"
+          value={friend}
+          onChange={(e) => setFriend(e.target.value)}
         />
 
         <button type="submit">Send</button>
