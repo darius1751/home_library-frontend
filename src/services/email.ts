4486b1933e95828dcdde6399261a8e9ef0601d58
Dani <dustyglass@gmail.com>
2024-11-19 18:58:22 -0300

    add last name

--- src/services/email.ts
+++ src/services/email.ts
@@ -1,6 +1,6 @@
 import { api } from "../config/api";
 
-export const sendBookEmail = async (sender: string, receiver: string, name:string, friend:string, id: string) => {
-    const response = await api.post('/email', { sender, receiver, name, friend, id })
+export const sendBookEmail = async (sender: string, receiver: string, name:string, friend:string, lastname:string, id: string) => {
+    const response = await api.post('/email', { sender, receiver, name, friend, lastname, id })
     return response.data
 }
