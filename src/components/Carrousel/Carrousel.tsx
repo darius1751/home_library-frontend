603b60593515fc3f77b6103b4f99485dde02de2c
darius1751 <lupequi12@gmail.com>
2024-11-24 17:44:01 -0500

    Create initial carousel

--- src/components/Carrousel/Carrousel.tsx
+++ src/components/Carrousel/Carrousel.tsx
@@ -1,17 +1,19 @@
-import { useRef } from 'react';
 import styles from './carrousel.module.css';
 type Props = {
     children: React.ReactNode[];
 }
 export const Carrousel = ({ children }: Props) => {
-    const $carrousel = useRef<HTMLDivElement>(null);
     return (
-        <div className={styles.carrousel} ref={$carrousel}>
-            {
-                children.map(
-                    (child, index) => <div className={`${styles.item}`} key={`carrousel-item-${index}`}>{child}</div>
-                )
-            }
+        <div className={styles.slider}>
+
+            <div className={styles.slideTrack}>
+                {
+                    children.map(
+                        (child, index) => <div className={`${styles.slide}`} key={`carrousel-item-${index}`}>{child}</div>
+                    )
+                }
+            </div>
+
         </div>
 
     )
