6e206aa3f08c0013fe93cf4a40f1726c47afa7cf
darius1751 <lupequi12@gmail.com>
2024-11-23 21:04:00 -0500

    Initial create carrousel

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,3 +1,5 @@
+import { Carrousel } from '../../components/Carrousel/Carrousel';
+import { Cite } from './components/Cite/Cite';
 import styles from './home.module.css';
 
 export const HomePage = () => {
@@ -5,7 +7,12 @@ export const HomePage = () => {
         <>
             <div className={`page ${styles.page}`}>
                 <div className={`container`}>
-
+                    <Carrousel>
+                        <Cite creator='darius1751' text='La programacion es mas bonita cuando piensas tu mismo el codigo 1'/>
+                        <Cite creator='darius1751' text='La programacion es mas bonita cuando piensas tu mismo el codigo 2'/>
+                        <Cite creator='darius1751' text='La programacion es mas bonita cuando piensas tu mismo el codigo 3'/>
+                        <Cite creator='darius1751' text='La programacion es mas bonita cuando piensas tu mismo el codigo 4'/>
+                    </Carrousel>
                 </div>
             </div>
         </>
