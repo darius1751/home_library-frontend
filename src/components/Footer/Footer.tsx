00a3fd31ee4b3a72fe05ea072e1a732ebdd6b9d0
darius1751 <lupequi12@gmail.com>
2024-11-30 11:29:14 -0500

    add cites and change footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,24 +1,18 @@
 import { NavLink } from 'react-router-dom';
-import { NavItem } from '../../interfaces/nav-item';
 import styles from './footer.module.css';
+import { Logo } from '../Logo/Logo';
 type Props = {
-    navItems: NavItem[];
     className?: string;
 }
-export const Footer = ({ navItems, className = "" }: Props) => {
+export const Footer = ({ className = "" }: Props) => {
     const year = new Date().getFullYear()
     return (
         <footer className={`${styles.footer} ${className}`}>
+            <Logo to='/' className={styles.logo} />
             <NavLink to="/" className={styles.title}>Home Library</NavLink>
-            <div className={styles.items}>
-                {
-                    navItems.map(({ text, to }, index) => (
-                        <li key={`footer-${index}`}>
-                            <NavLink to={to} className={styles.item}>{text}</NavLink>
-                        </li>
-                    )
-                    )}
-            </div>
+            <NavLink to={'tel://+56979298644'} className={styles.phone}>
+                Phone: <span className={styles.detail}>+56979298644</span>
+            </NavLink>
             <p className={styles.email}>homeLibrary@email.com</p>
             <small className={styles.copyright}>copyright©{year}</small>
         </footer>
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,9 +2,10 @@ import { NavLink } from 'react-router-dom';
 import styles from './logo.module.css';
 type Props = {
     to?: string;
+    className?: string;
 }
-export const Logo = ({ to }: Props) => {
+export const Logo = ({ to, className }: Props) => {
     return (
-        <NavLink to={to ?? '/'} className={styles.logo} />
+        <NavLink to={to ?? '/'} className={`${styles.logo} ${className}`} />
     )
 }
